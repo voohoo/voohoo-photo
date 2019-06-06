7ea567d351ad78350d68d210aaa514ec17353302
Vuhuy Pham <vuhuypham99@gmail.com>
2019-06-05 20:24:36 -0700

    Remove replace autogenerated template with basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Grid from '@material-ui/core/Grid';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container spacing={0} style={{height: "100vh"}}>
+      <Grid item md={4} className="left">
+        
+      </Grid>
+      <Grid item md={8}>
+        
+      </Grid>
+    </Grid>
   );
 }
 
