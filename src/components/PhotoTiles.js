994d36fa22efe96bfbb2b33d349dd67e595eacc4
Vuhuy Pham <vuhuypham99@gmail.com>
2019-06-06 15:19:16 -0700

    Create PhotoTiles

---
+++ src/components/PhotoTiles.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import tileData from './tileData';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: '100%',
+    height: '100%',
+  },
+}));
+
+function PhotoTiles() {
+  const classes = useStyles();
+
+  console.log(tileData);
+
+  return (
+    <div className={classes.root}>
+      <GridList cellHeight={160} className={classes.gridList} cols={3}>
+        {tileData.map(tile => (
+          <GridListTile key={tile.img} cols={tile.cols || 1}>
+            <img src={tile.img} alt={tile.title} />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
+}
+
+export default PhotoTiles;
