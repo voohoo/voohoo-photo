bb72a82c75ab9445150f209f098449e82a24c645
Vuhuy Pham <vuhuypham99@gmail.com>
2019-06-06 15:19:40 -0700

    Import and implement TopBar and PhotoTiles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Grid from '@material-ui/core/Grid';
+import TopBar from './components/TopBar';
+import PhotoTiles from './components/PhotoTiles';
 
 function App() {
   return (
     <Grid container spacing={0} style={{height: "100vh"}}>
       <Grid item md={4} className="left">
-        
+        <TopBar></TopBar>
+        <PhotoTiles></PhotoTiles>
       </Grid>
       <Grid item md={8}>
         
