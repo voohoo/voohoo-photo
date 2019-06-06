4f622a93cd1fff10632034fe82f9b28f08d3135e
Vuhuy Pham <vuhuypham99@gmail.com>
2019-06-06 15:17:55 -0700

    Add /images and create tileData

---
+++ src/components/tileData.js
@@ -0,0 +1,94 @@
+const tileData = [
+  {
+    img: 'images/01.jpg',
+    title: 'Image',
+    author: 'author',
+    cols: 2,
+  },
+  {
+    img: 'images/02.jpg',
+    title: 'Image',
+    author: 'author',
+    cols: 1,
+  },
+  {
+    img: 'images/03.jpg',
+    title: 'Image',
+    author: 'author',
+    cols: 1,
+  },
+  {
+    img: 'images/04.jpg',
+    title: 'Image',
+    author: 'author',
+    cols: 2,
+  },
+  {
+    img: 'images/05.jpg',
+    title: 'Image',
+    author: 'author',
+    cols: 1,
+  },
+  {
+    img: 'images/06.jpg',
+    title: 'Image',
+    author: 'author',
+    cols: 1,
+  },
+  {
+    img: 'images/07.jpg',
+    title: 'Image',
+    author: 'author',
+    cols: 1,
+  },
+  {
+    img: 'images/08.jpg',
+    title: 'Image',
+    author: 'author',
+    cols: 2,
+  },
+  {
+    img: 'images/09.jpg',
+    title: 'Image',
+    author: 'author',
+    cols: 1,
+  },
+  {
+    img: 'images/10.jpg',
+    title: 'Image',
+    author: 'author',
+    cols: 1,
+  },
+  {
+    img: 'images/11.jpg',
+    title: 'Image',
+    author: 'author',
+    cols: 1,
+  },
+  {
+    img: 'images/12.jpg',
+    title: 'Image',
+    author: 'author',
+    cols: 1,
+  },
+  {
+    img: 'images/13.jpg',
+    title: 'Image',
+    author: 'author',
+    cols: 1,
+  },
+  {
+    img: 'images/14.jpg',
+    title: 'Image',
+    author: 'author',
+    cols: 1,
+  },
+  {
+    img: 'images/15.jpg',
+    title: 'Image',
+    author: 'author',
+    cols: 1,
+  }
+];
+
+export default tileData;
