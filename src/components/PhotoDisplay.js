143ea4c6547c79f9e0865de09d6fd4f626b91832
Vuhuy Pham <vuhuypham99@gmail.com>
2019-10-16 20:18:27 -0700

    Implement full screen photo display

--- src/components/PhotoDisplay.js
+++ src/components/PhotoDisplay.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './PhotoDisplay.css';
 
-function PhotoDisplay(props) {
-  return (
-    <div className="photo-container">
-      <img src={props.photoPath} className="photo-display" alt="Displayed" />
-    </div>
-  );
+class PhotoDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFullScreen: false,
+    };
+  }
+
+  handleClick() {
+    console.log('CLICK');
+    this.setState(prevState => ({
+      isFullScreen: !prevState.isFullScreen
+    }));
+  }
+
+  render() {
+    console.log('RENDER');
+    console.log(this.state.isFullScreen);
+    const displayClass =
+      this.state.isFullScreen ? "photo-container full-screen" : "photo-container";
+
+    return (
+      <div className={displayClass} onClick={() => this.handleClick()}>
+        <img src={this.props.photoPath} className="photo-display" alt="Displayed" />
+      </div>
+    );
+  }
 }
 
 export default PhotoDisplay;
