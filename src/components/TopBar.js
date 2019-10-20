170e48b4b835bf57ffc46f4442405227697229f4
Vuhuy Pham <vuhuypham99@gmail.com>
2019-10-19 22:57:49 -0700

    Add menu bars

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -5,6 +5,7 @@ function TopBar() {
   return (
     <div id="top-bar">
       <div className="top-bar-content">
+        <i id="menu-bars" class="fas fa-bars"></i>
         <img src="images/brand/voohoo-black.png" width="100" alt="voohoo brand" />
       </div>
     </div>
